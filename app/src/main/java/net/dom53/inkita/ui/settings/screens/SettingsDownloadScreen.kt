ab947015aa6b5288a2c220fb8a0b2dda744990e0
dom53 <mandinec53@gmail.com>
2025-12-18 16:12:49 +0100

    Fix import order in SettingsDownloadScreen.kt

--- app/src/main/java/net/dom53/inkita/ui/settings/screens/SettingsDownloadScreen.kt
+++ app/src/main/java/net/dom53/inkita/ui/settings/screens/SettingsDownloadScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.ui.res.stringResource
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -36,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
